e23496d067f9ae6c465a47dba67ea5a939c8dc37
MaksymIzotov <maksym.izotov02@gmail.com>
2022-11-10 11:06:56 +0900

    settings info added

--- RPG Dungeon Crawler/Assets/Scripts/Settings/BrightnessSetings.cs
+++ RPG Dungeon Crawler/Assets/Scripts/Settings/BrightnessSetings.cs
@@ -1,11 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DigitalRuby.SimpleLUT;
 
 public class BrightnessSetings : MonoBehaviour
 {
-    public void ChangeBrightness()
+    [SerializeField] private CurrentSettings settings;
+    SimpleLUT sl;
+
+    private void Start()
     {
-       
+        sl = GetComponent<SimpleLUT>();
+
+        SetupBrightness();
+    }
+
+    private void SetupBrightness()
+    {
+        sl.Brightness = settings.brightness;
     }
 }
