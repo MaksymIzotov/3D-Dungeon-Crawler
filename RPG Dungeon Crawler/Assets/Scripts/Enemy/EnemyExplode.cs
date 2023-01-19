96a50dd446849650196984edcd730b57fe04ed72
MaksymIzotov <maksym.izotov02@gmail.com>
2023-01-19 18:13:54 +0900

    golem added

--- RPG Dungeon Crawler/Assets/Scripts/Enemy/EnemyExplode.cs
+++ RPG Dungeon Crawler/Assets/Scripts/Enemy/EnemyExplode.cs
@@ -12,8 +12,18 @@ public class EnemyExplode : MonoBehaviour
     {
         for (int i = 0; i < bodyParts.Length; i++)
         {
+            bodyParts[i].transform.parent = bodyParts[i].transform.root;
             bodyParts[i].GetComponent<Rigidbody>().isKinematic = false;
-            bodyParts[i].GetComponent<CapsuleCollider>().enabled = true;
+
+
+            if(bodyParts[i].GetComponent<CapsuleCollider>() != null) 
+                bodyParts[i].GetComponent<CapsuleCollider>().enabled = true;
+            else if(bodyParts[i].GetComponent<SphereCollider>() != null)
+                bodyParts[i].GetComponent<SphereCollider>().enabled = true;
+            else if(bodyParts[i].GetComponent<MeshCollider>() != null)
+                bodyParts[i].GetComponent<MeshCollider>().enabled = true;
+
+
             bodyParts[i].GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(0f,1f), Random.Range(0f, 1f), Random.Range(0f, 1f)) * explosionForce, ForceMode.Impulse);
             bodyParts[i].GetComponent<Rigidbody>().AddTorque(new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)) * rotationalForce, ForceMode.Impulse);
         }
