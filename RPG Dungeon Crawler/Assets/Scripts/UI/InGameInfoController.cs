824a0c3706791a7cea44fb0e838e7235bc4c5ad8
MaksymIzotov <maksym.izotov02@gmail.com>
2023-03-22 20:50:52 +0900

    minimap updated

--- RPG Dungeon Crawler/Assets/Scripts/UI/InGameInfoController.cs
+++ RPG Dungeon Crawler/Assets/Scripts/UI/InGameInfoController.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
-using TreeEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class InGameInfoController : MonoBehaviour
 {
+    GameObject minimapCamera;
+
     private void Start()
     {
-        GameObject minimapCamera = GameObject.Find("MinimapCamera");
+        minimapCamera = GameObject.Find("MinimapCamera");
         minimapCamera.transform.position = new Vector3(transform.position.x, minimapCamera.transform.position.y, transform.position.z);
         minimapCamera.transform.parent = transform;
     }
@@ -16,6 +17,11 @@ public class InGameInfoController : MonoBehaviour
     public void ToggleOn(InputAction.CallbackContext context)
     {
         PassiveDescriptionShow.Instance.Toggle(true);
+
+        minimapCamera.GetComponent<Camera>().orthographicSize = 200;
+        minimapCamera.transform.parent = null;
+        minimapCamera.transform.position = new Vector3(125, minimapCamera.transform.position.y, 125);
+        minimapCamera.transform.eulerAngles = new Vector3(90, 0, 0);
 
         //Showing hints for totems
         List<GameObject> totems = LevelManager.Instance.GetCurrentTotems;
@@ -30,6 +36,10 @@ public class InGameInfoController : MonoBehaviour
     public void ToggleOff(InputAction.CallbackContext context)
     {
         PassiveDescriptionShow.Instance.Toggle(false);
+
+        minimapCamera.GetComponent<Camera>().orthographicSize = 150;
+        minimapCamera.transform.position = new Vector3(transform.position.x, minimapCamera.transform.position.y, transform.position.z);
+        minimapCamera.transform.parent = transform;
 
         //Showing hints for totems
         List<GameObject> totems = LevelManager.Instance.GetCurrentTotems;
