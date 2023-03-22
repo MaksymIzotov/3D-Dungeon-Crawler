a63bc7032bb124dd4f766326cfc5bd425606c93c
MaksymIzotov <maksym.izotov02@gmail.com>
2023-03-22 17:36:09 +0900

    minimap added

--- RPG Dungeon Crawler/Assets/Scripts/UI/InGameInfoController.cs
+++ RPG Dungeon Crawler/Assets/Scripts/UI/InGameInfoController.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TreeEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class InGameInfoController : MonoBehaviour
 {
+    private void Start()
+    {
+        GameObject minimapCamera = GameObject.Find("MinimapCamera");
+        minimapCamera.transform.position = new Vector3(transform.position.x, minimapCamera.transform.position.y, transform.position.z);
+        minimapCamera.transform.parent = transform;
+    }
+
     public void ToggleOn(InputAction.CallbackContext context)
     {
         PassiveDescriptionShow.Instance.Toggle(true);
