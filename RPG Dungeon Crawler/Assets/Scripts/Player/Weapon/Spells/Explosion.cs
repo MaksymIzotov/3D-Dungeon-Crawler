d9d4615118bc26bc644b9d55939df3af0b3e569c
MaksymIzotov <maksym.izotov02@gmail.com>
2023-03-24 10:23:47 +0900

    explosion spell added

---
+++ RPG Dungeon Crawler/Assets/Scripts/Player/Weapon/Spells/Explosion.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Explosion", menuName = "Spells/Explosion", order = 1)]
+public class Explosion : Spell
+{
+    [Space(10)]
+    [Header("Main properties")]
+    public GameObject explosionFX;
+
+    public float damage;
+
+    [Space(10)]
+    [Header("Upgrade amount")]
+    public float damageMult;
+    public float cooldownReduce;
+    public float priceMult;
+
+    [Space(20)]
+    [Header("Spell default stats")]
+    public float def_damage;
+    public float def_cooldown;
+    public int def_upgradePrice;
+
+    public override void PreCast(Transform spellSpawnpoint)
+    {
+        //Animation
+        GameObject player = spellSpawnpoint.root.gameObject;
+        player.GetComponent<AnimationManager>().PlayPlayerAnimation(ANIMATIONS.EXPLOSION_ANIM);
+
+        //Play audio (riser)
+    }
+
+    public override void Cast(Transform spellSpawnpoint)
+    {
+        GameObject player = spellSpawnpoint.root.gameObject;
+        Instantiate(explosionFX, player.transform.position, Quaternion.identity);
+
+        float criticalMult = player.GetComponent<PlayerPassives>().TryCriticalDamage();
+
+        //Do damage
+        var collidersInRange = Physics.OverlapSphere(player.transform.position, 10);
+
+        foreach (Collider n in collidersInRange)
+        {
+            if (n.tag == TAGS.ENEMY_TAG)
+            {
+                if (player.GetComponent<PlayerPassives>().TryInstaKill())
+                    n.transform.root.GetComponent<IDamagable>()?.TakeDamage(999999, player);
+                else
+                    n.transform.root.GetComponent<IDamagable>()?.TakeDamage(damage * criticalMult, player);
+
+                if (player.GetComponent<PlayerPassives>().GetBurnDamage() > 0)
+                {
+                    player.GetComponent<ParticlesController>().SpawnBurnParticles(n.transform.root, 3f, player.GetComponent<PlayerPassives>().GetBurnDamage());
+                }
+            }
+        }
+
+        // Play audio (explosion)
+
+    }
+
+    public override void CastUpgraded(Transform spellProperty)
+    {
+
+    }
+
+    public override string Stats()
+    {
+        return "Damage: " + damage.ToString("F") + "\nCooldown: " + coolDownTime.ToString("F");
+    }
+
+    public override string Desription()
+    {
+        return "Creates an explosion around the player damaging everyone around";
+    }
+
+    public override void UpgradeStats()
+    {
+        float newPrice = upgradePrice * priceMult;
+        upgradePrice = (int)newPrice;
+
+        coolDownTime -= cooldownReduce;
+        damage *= damageMult;
+    }
+
+    public override void Reset()
+    {
+        damage = def_damage;
+        coolDownTime = def_cooldown;
+        upgradePrice = def_upgradePrice;
+
+        lvl = 1;
+    }
+}
--- RPG Dungeon Crawler/Assets/Scripts/SharedScripts/ANIMATIONS.cs
+++ RPG Dungeon Crawler/Assets/Scripts/SharedScripts/ANIMATIONS.cs
@@ -8,4 +8,5 @@ public static class ANIMATIONS
     public static string HEALINGCROSS_ANIM = "Healing";
     public static string HEALINGPOTION_ANIM = "HealingPotionUse";
     public static string BOMB_ANIM = "BombUse";
+    public static string EXPLOSION_ANIM = "Explosion";
 }
